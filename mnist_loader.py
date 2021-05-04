631e0ac715945edef8af61f082d517bda5323403
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-05-04 13:40:00 +0530

    Played around with the MNIST datafile, tailored NN to be more digit-centric

---
+++ mnist_loader.py
@@ -0,0 +1,54 @@
+import io
+
+with open("data/t10k-labels-idx1-ubyte", "rb") as f:
+	bytes_read = f.read()
+
+stream = io.BytesIO(bytes_read)
+
+nums = []
+
+a = int.from_bytes(stream.read(4), byteorder='big')
+if a == 2049:
+	s = int.from_bytes(stream.read(4), byteorder='big')
+	print(s)
+
+	while True:
+		b = stream.read(1)
+		if not b:
+			break
+		nums.append(ord(b))
+
+with open("data/t10k-images-idx3-ubyte", "rb") as f:
+	img_bytes = f.read()
+
+stream = io.BytesIO(img_bytes)
+
+a = int.from_bytes(stream.read(4), byteorder='big')
+if a == 2051:
+	s = int.from_bytes(stream.read(4), byteorder='big')
+	h = int.from_bytes(stream.read(4), byteorder='big')
+	w = int.from_bytes(stream.read(4), byteorder='big')
+
+	print(s)
+	print(h)
+	print(w)
+	
+	ctr = 0;
+	while ctr < s:
+		img = io.BytesIO()
+		# bitmap header
+		img.write(b"\x42\x4D\x00\x00\x00\x00\x00\x00\x00\x00\x36\x04\x00\x00\x28\x00\x00\x00\x1C\x00\x00\x00\x1C\x00\x00\x00\x01\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\x00\x00\x00\x00\x00\x00\x00")
+		for k in range(256):
+			img.write(bytearray([k,k,k,0]))
+		rows = []
+		for i in range(h):
+			row = stream.read(w)
+			rows.insert(0,row)
+		for i in range(h):
+			img.write(rows[i])
+		
+		out = open(f"img/{ctr:05d}_{nums[ctr]}.bmp", "wb")
+		img.seek(0)
+		out.write(img.read())
+		print(f"Wrote to file img/{ctr:05d}_{nums[ctr]}.bmp")
+		ctr += 1
