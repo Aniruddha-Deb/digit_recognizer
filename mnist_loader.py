2818c148eb4de88eaed089945910823a8c15c013
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-05-04 17:31:07 +0530

    No change in weights, oof! need to debug

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,54 +1,102 @@
 import io
+import numpy as np
 
-with open("data/t10k-labels-idx1-ubyte", "rb") as f:
-	bytes_read = f.read()
+class DataLoader:
 
-stream = io.BytesIO(bytes_read)
-
-nums = []
-
-a = int.from_bytes(stream.read(4), byteorder='big')
-if a == 2049:
-	s = int.from_bytes(stream.read(4), byteorder='big')
-	print(s)
-
-	while True:
-		b = stream.read(1)
-		if not b:
-			break
-		nums.append(ord(b))
-
-with open("data/t10k-images-idx3-ubyte", "rb") as f:
-	img_bytes = f.read()
-
-stream = io.BytesIO(img_bytes)
-
-a = int.from_bytes(stream.read(4), byteorder='big')
-if a == 2051:
-	s = int.from_bytes(stream.read(4), byteorder='big')
-	h = int.from_bytes(stream.read(4), byteorder='big')
-	w = int.from_bytes(stream.read(4), byteorder='big')
-
-	print(s)
-	print(h)
-	print(w)
-	
-	ctr = 0;
-	while ctr < s:
-		img = io.BytesIO()
-		# bitmap header
-		img.write(b"\x42\x4D\x00\x00\x00\x00\x00\x00\x00\x00\x36\x04\x00\x00\x28\x00\x00\x00\x1C\x00\x00\x00\x1C\x00\x00\x00\x01\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\x00\x00\x00\x00\x00\x00\x00")
-		for k in range(256):
-			img.write(bytearray([k,k,k,0]))
-		rows = []
-		for i in range(h):
-			row = stream.read(w)
-			rows.insert(0,row)
-		for i in range(h):
-			img.write(rows[i])
+	def load_labels(self, file_loc, limit):
+		with open(file_loc, "rb") as f:
+			bytes_read = f.read()
 		
-		out = open(f"img/{ctr:05d}_{nums[ctr]}.bmp", "wb")
-		img.seek(0)
-		out.write(img.read())
-		print(f"Wrote to file img/{ctr:05d}_{nums[ctr]}.bmp")
-		ctr += 1
+		stream = io.BytesIO(bytes_read)
+		nums = []
+		a = int.from_bytes(stream.read(4), byteorder='big')
+		if a == 2049:
+			s = int.from_bytes(stream.read(4), byteorder='big')
+		
+			if limit:
+				s = limit
+			print(s)
+			for i in range(s):
+				b = stream.read(1)
+				nums.append(self.vectorize(ord(b)))
+			return nums
+		else:
+			print("Error while loading labels: checksum doesn't match")
+			return None
+	
+	def vectorize(self, j):
+		e = np.zeros((10,1))
+		e[j] = 1
+		return e
+
+	def load_images(self, file_loc, limit):
+		with open(file_loc, "rb") as f:
+			img_bytes = f.read()
+
+		stream = io.BytesIO(img_bytes)
+		
+		images = []
+		a = int.from_bytes(stream.read(4), byteorder='big')
+		if a == 2051:
+			s = int.from_bytes(stream.read(4), byteorder='big')
+			h = int.from_bytes(stream.read(4), byteorder='big')
+			w = int.from_bytes(stream.read(4), byteorder='big')
+			
+			if limit:
+				s = limit
+
+			for i in range(s):
+				image = []
+				for j in range(h*w):
+					b = int.from_bytes(stream.read(1), byteorder='big')
+					image.append(float(b)/255)
+				images.append(np.array([image]).transpose())
+			return images
+		else:
+			print("Error while loading images: checksum doesn't match")
+			return None
+	
+	def load_test_set(self, limit=None):
+		return list(zip(self.load_images("data/t10k-images-idx3-ubyte", limit),
+						self.load_labels("data/t10k-labels-idx1-ubyte", limit)))
+	
+	def load_training_set(self, limit=None):
+		return list(zip(self.load_images("data/train-images-idx3-ubyte", limit),
+						self.load_labels("data/train-labels-idx1-ubyte", limit)))
+
+
+
+#with open("data/t10k-images-idx3-ubyte", "rb") as f:
+#	img_bytes = f.read()
+#
+#stream = io.BytesIO(img_bytes)
+#
+#a = int.from_bytes(stream.read(4), byteorder='big')
+#if a == 2051:
+#	s = int.from_bytes(stream.read(4), byteorder='big')
+#	h = int.from_bytes(stream.read(4), byteorder='big')
+#	w = int.from_bytes(stream.read(4), byteorder='big')
+#
+#	print(s)
+#	print(h)
+#	print(w)
+#	
+#	ctr = 0;
+#	while ctr < s:
+#		img = io.BytesIO()
+#		# bitmap header
+#		img.write(b"\x42\x4D\x00\x00\x00\x00\x00\x00\x00\x00\x36\x04\x00\x00\x28\x00\x00\x00\x1C\x00\x00\x00\x1C\x00\x00\x00\x01\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\x00\x00\x00\x00\x00\x00\x00")
+#		for k in range(256):
+#			img.write(bytearray([k,k,k,0]))
+#		rows = []
+#		for i in range(h):
+#			row = stream.read(w)
+#			rows.insert(0,row)
+#		for i in range(h):
+#			img.write(rows[i])
+#		
+#		out = open(f"img/{ctr:05d}_{nums[ctr]}.bmp", "wb")
+#		img.seek(0)
+#		out.write(img.read())
+#		print(f"Wrote to file img/{ctr:05d}_{nums[ctr]}.bmp")
+#		ctr += 1
---
+++ network.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class Network:
+	
+	# this is all mostly taken from Nielsen's book
+	def __init__(self, sizes):
+		self.num_layers = len(sizes)
+		self.sizes = sizes
+		self.biases = [np.random.rand(y,1) for y in sizes[1:]]
+		self.weights = [np.random.rand(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+
+	def feedforward(self, a):
+		for b, w in zip(self.biases, self.weights):
+			a = relu(np.dot(w, a)+b)
+		return a
+	
+	def backpropagate(self, x, y):
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+		activation = x
+		activations = [x]
+		# z_i = w_ia_i + b_i
+		z_vecs = []
+		# feedforward and record activations
+		for b, w in zip(self.biases, self.weights):
+			z = np.dot(w, activation) + b
+			z_vecs.append(z)
+			activation = relu(z)
+			activations.append(activation)
+
+		# backpropagate
+		delta = cost_dv(activations[-1], y)*relu_prime(z_vecs[-1])
+		nabla_w[-1] = np.dot(delta, activations[-2].transpose()) # wow!
+
+		for l in range(2, self.num_layers):
+			z = z_vecs[-l]
+			# just doing the same thing as before, only iteratively now
+			delta = np.dot(self.weights[-l+1].transpose(), delta)*relu_prime(z)
+			nabla_b[-l] = delta
+			nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+		return (nabla_b, nabla_w)
+
+	def train(self, training_data, epochs, batch_size, eta, test_data=None):
+
+		if test_data:
+			n_test = len(test_data)
+		n = len(training_data)
+
+		for j in range(epochs):
+			np.random.shuffle(training_data)
+			batches = [training_data[k:k+batch_size] for k in range(0,n,batch_size)]
+			for batch in batches:
+				self.update(batch, eta)
+
+			if test_data:
+				print(f"Epoch {j}: {self.evaluate(test_data)} / {n_test}")
+			else:
+				print(f"Epoch {j} complete")
+
+	def update(self, batch, eta):
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+		for x,y in batch:
+			delta_nabla_b, delta_nabla_w = self.backpropagate(x,y)
+			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+			nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+		n = len(batch)
+		self.weights = [w-(eta/n)*nw for w, nw in zip(self.weights, nabla_w)]
+		self.biases = [b-(eta/n)*nb for b, nb in zip(self.biases, nabla_b)]
+
+	def evaluate(self, test_data):
+		results = [(np.argmax(self.feedforward(x)),np.argmax(y)) for (x,y) in test_data]
+		return sum(int(x==y) for (x,y) in results)
+
+def cost_dv(output, y):
+	return output-y
+
+def relu(x):
+	return np.maximum(x,0)
+
+def relu_prime(x):
+	return np.greater(x,0).astype(int)
+
